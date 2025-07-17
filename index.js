c082a97421f080f46280f7d45818a2efca15f222
lolodev <horaciodev1706@gmail.com>
2025-07-16 22:29:58 -0300

    BlogSpace - Display blogs on page

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
+const containerEl = document.querySelector('#container');
+let postHtml = '';
+
 fetch('https://apis.scrimba.com/jsonplaceholder/posts')
     .then(response => response.json())
     .then(data => {
-        console.log(data.slice(0, 5));
+        const postsArr = data.slice(0, 5);
+        // console.log(postsArr);
+        postsArr.forEach(post => {
+            // console.log(post);
+            // console.log(post.title);
+            postHtml += `
+            <h2 class="post-title">${post.title}</h2>
+            <p class="post-body">${post.body}</p>
+             `;
+        });
+        containerEl.innerHTML = postHtml;
     });
+
